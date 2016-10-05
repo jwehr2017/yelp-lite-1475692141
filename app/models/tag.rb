cabdc3c0aff1e5d09ed62f8e0bfd6ea7aa3501a9
Raghu Betina <raghu@firstdraft.com>
2016-10-05 18:34:41 +0000

    Direct association between tags and restaurant_tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,10 @@
 class Tag < ApplicationRecord
   # Direct associations
 
+  has_many   :restaurant_tags,
+             :foreign_key => "tags_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
